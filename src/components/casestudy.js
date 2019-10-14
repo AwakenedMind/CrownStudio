7a2ae7793d723d9753501d40dba98ad3f52f0d2e
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-10-14 21:14:44 +1000

    case-study

---
+++ src/components/casestudy.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Study = () => {
+    return (
+    <div class="Study-wrapper">
+        <h1>Case Studies</h1>
+        <div class="Study-info">
+        <p class="Company-name">Interattiva</p>
+        <p class="Company-review">I have gotten so much valuable customers using CrownStudio. I can't say enough about the design. The service was excellent. Completely surpassed our expectations at Interattiva.</p>
+        <p class="Company-ceo">Jon Sith, CEO</p>
+        </div>
+    </div>
+    )
+}
+
+export default Study
