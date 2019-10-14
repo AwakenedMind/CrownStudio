5ae3f12213513259d9389bfb4439d826ec1524cd
Robert A <daspanisharmada@gmail.com>
2019-10-14 02:54:52 -0700

    added technologies and services component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './sass/style.scss';
 import Landing from './components/landing'
+import Technologies from './components/technologies'
+import Services from './components/services'
 
 function App() {
   return (
     <div>
       <Landing />
+      <Technologies />
+      <Services />
     </div>
   );
 }
