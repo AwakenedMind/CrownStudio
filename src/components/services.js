9ffeea902bb8dbefdd2ef0c6a34ec82c61ec4e20
Robert A <daspanisharmada@gmail.com>
2019-10-15 05:51:05 -0700

    added services component

--- src/components/services.js
+++ src/components/services.js
@@ -1,21 +1,52 @@
 import React from 'react'
 import WebDesign from '../images/Webdesign.svg'
+import WebDevelopment from '../images/WebDevelopment.svg'
+import SEO from '../images/SEO.svg'
+import ServiceIllustration from '../images/service-illustration.svg'
+import '../sass/services.scss'
 
 const Services = () => {
   return (
-    <div>
-      <h2>Our Services</h2>
-      <hr />
-      <div>
-      <div>
-        <img src={WebDesign} />
-        <h4>Web Design </h4>
+    <section className="services-wrapper">
+      <h2 className="services-header">Our Services</h2>
+      <div className="services-grid">
+        <ul className="services-list">
+          <li className="services-list-item">
+            <div className="services-wrap">
+              <img src={WebDesign} className="services-illustration" alt="crownstudio-web-design-service" />
+              <h4 className="services-heading">Web Design </h4>
+            </div>
+            <p className="services-description">
+              Prototyped from scratch using industry standard software. Visually stunning sites that follow rigid UI/UX practices. Design that aligns with Your brand & Identity.
+            </p>
+          </li>
+
+          <li className="services-list-item">
+            <div className="services-wrap">
+              <img src={WebDevelopment} className="services-illustration" alt="crownstudio-web-development-service" />
+              <h4 className="services-heading">Web Development </h4>
+            </div>
+            <p className="services-description">
+              Developed from scratch to fit your company’s brand.
+              Same technology used by Fortune 100 companies.
+            </p>
+          </li>
+
+          <li className="services-list-item">
+            <div className="services-wrap">
+              <img src={SEO} className="services-illustration" alt="crownstudio-seo-service" />
+              <h4 className="services-heading">SEO</h4>
+            </div>
+            <p className="services-description">
+              PPC Campaigns for high quality leads.
+              Optimized SEO that drive organic traffic.
+              Competitor Research for SEO Strategy.
+            </p>
+          </li>
+        </ul>
+        <img src={ServiceIllustration} />
       </div>
-      <p>
-      Prototyped from scratch using industry standard software Visually stunning sites that follow rigid UI/uX practices Design that aligns with Your brand & Identity
-      </p>
-        </div>
-    </div>
+    </section>
   )
 }
 
