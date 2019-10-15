c319ca84ea9d744ee9b9411f130ede01a675fedb
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-10-15 21:55:57 +1000

    casestudy2

--- src/components/casestudy.js
+++ src/components/casestudy.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import LeftArrow from '../images/leftarrow-icon.svg'
+import RightArrow from '../images/rightarrow-icon.svg'
 
 const Study = () => {
     return (
@@ -8,6 +10,11 @@ const Study = () => {
         <p class="Company-name">Interattiva</p>
         <p class="Company-review">I have gotten so much valuable customers using CrownStudio. I can't say enough about the design. The service was excellent. Completely surpassed our expectations at Interattiva.</p>
         <p class="Company-ceo">Jon Sith, CEO</p>
+        <div className="Numbers">
+        <img className ="Leftarrow" alt ="left Arrow"src = {LeftArrow}></img>
+        <p className="numbers-text">03/05</p>
+        <img className ="RightArrow" alt ="Right Arrow"src = {RightArrow}></img>
+            </div>
         </div>
     </div>
     )
